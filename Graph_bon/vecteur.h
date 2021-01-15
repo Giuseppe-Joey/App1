408d5ec6935ff33c2bab935f52efd439be274dec
Joey <Guiseppe.Lomonaco@usherbrooke.ca>
2021-01-15 15:08:08 -0500

    update

--- Graph_bon/vecteur.h
+++ Graph_bon/vecteur.h
@@ -6,6 +6,20 @@
 
 class vecteur
 {
-		
+	public:
+	int x;
+	int y;
+	int z;
+	
+	
+	vecteur()
+	{
+		std::cout << "Construction de Vecteur a l'adresse : " << this << std::endl;
+	}
+	
+	~vecteur()
+	{
+		std::cout << "Destruction de Vecteur a l'adresse : " << this << std::endl;
+	}	
 
 };
