216a4c0ff8894e7c53d1909cafa2c88b47abd744
Bertrand935 <labb3102@usherbrooke.ca>
2021-01-18 17:32:16 -0500

    debut de diagramme uml

--- vecteur_bert/pseudocode_fonction_ajouter_vecteur.cpp
+++ vecteur_bert/pseudocode_fonction_ajouter_vecteur.cpp
@@ -10,23 +10,23 @@ DEBUT
 	
 	verification := true;
 	
-	SI taille = capacite  ALORS
+	SI taille := capacite  ALORS
 	
-		verification = add_capacity();
+		verification := add_capacity();
 	
 	{
 		newTab(Forme**) := Forme*[capacite *2](new Forme*);  //création de newTab dans la heap qui est de même type que le tableau,
-								       // puis doubler la capacite du tableau
+								      // puis doubler la capacite du tableau
 		
 		POUR i := 1 À taille PAR PAS DE 1
 		
-		{
+		
 			newTab[i] := tableau[i]; //newTab devient une copie du tableau
-		}
+		
 		
 		delete[] tableau; //détruit toute les valeurs du tableau
 		
-		tableau = newTab  //le tableau copie le valeurs de newTab
+		tableau = newTab;  //le tableau copie le valeurs de newTab
 		
 		SI tableau = nullptr	ALORS
 			
